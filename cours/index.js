887d5735855e5ee2892de983ac4571edfb6fba61
maedial <adrien.lomas@maedial.com>
2022-06-21 12:12:33 +0200

    key events, mouse events, son...

--- cours/index.js
+++ cours/index.js
@@ -27,7 +27,6 @@ window.addEventListener('mousemove', (e) => {
     mousemove.style.top = e.pageY +'px';
     mousemove.style.left = e.pageX +'px';
 });
-
 window.addEventListener('mousedown',()=> {
     mousemove.style.transform = 'scale(2) translate(-25%, -25%)';
     mousemove.style.border = '2px solid blue';
@@ -36,4 +35,63 @@ window.addEventListener('mouseup',()=> {
     mousemove.style.transform = 'scale(1) translate(-50%, -50%)';
     mousemove.style.border = '2px solid teal';
 })
+questionContainer.addEventListener('mouseenter',() => {
+    questionContainer.style.background = "rgba(0,0,0,0.6)";
+})
+questionContainer.addEventListener('mouseout',() => {
+    questionContainer.style.background = "rgba(0,0,0,1)";
+})
+response.addEventListener('mouseover', () => {
+    response.style.transform = 'rotate(2deg)';
+})
+//------------------------------------------------------------------
+//Key press event
+//------------------------------------------------------------
+const keypressContainer = document.querySelector('.keypress');
+const key = document.getElementById('key');
 
+const ring = () => {
+    const audio = new Audio();
+    audio.src ='../supports de cours/cours/Enter.mp3';
+    audio.play();
+}
+
+document.addEventListener('keypress', (e) => {
+    key.textContent = e.key;    //permet d'insérer du texte dans un input par exemple
+    //ring();
+})
+//------------------------------------------------------------------
+//scroll event
+//------------------------------------------------------------
+const nav = document.querySelector("nav");
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 120) nav.style.top = 0;
+    else nav.style.top = '-50px';
+})
+//------------------------------------------------------------------
+// Form events
+const inputName = document.querySelector('input[type="text"]');
+const select = document.querySelector('select');
+const form = document.querySelector('form');
+let pseudo = '';
+let language = '';
+
+inputName.addEventListener('input', (e) => {
+    pseudo = e.target.value;
+})
+select.addEventListener('input', (e) => {
+    language = e.target.value;
+})
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    if(cgv.checked){
+        document.querySelector('form > div').innerHTML = `
+        <h3>Pseudo : ${pseudo}</h3>
+        <h4>language préféré : ${language}</h4>
+        `;   //pas aussi classe que de passer par une varialbe mais fonctionne, permet d'injecter du HTML... balise etc...
+    }else{
+        alert('Veuillez accepter les CGV');
+    }
+})
