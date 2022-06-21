de17f7ce1bd62f7b41a499237bfa3a5aeac17e76
maedial <adrien.lomas@maedial.com>
2022-06-21 17:04:14 +0200

    exercice tp2.2 terminé

--- tp2.2/index.js
+++ tp2.2/index.js
@@ -5,3 +5,14 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+const navbar = document.getElementById('navbar');
+let scrollPosition = 0;
+
+window.addEventListener('scroll',()=>{
+    if(scrollPosition>window.scrollY)
+        navbar.style.top = '0px';
+    else 
+        navbar.style.top = '-60px';
+    
+    scrollPosition=window.scrollY;
+})
