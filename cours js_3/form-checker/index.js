87bca2d21c40ee7f2babace3e6f665c18279b448
maedial <adrien.lomas@maedial.com>
2022-06-24 14:07:10 +0200

    fin du cours 3, TP à suivre !

--- cours js_3/form-checker/index.js
+++ cours js_3/form-checker/index.js
@@ -1,10 +1,12 @@
+const form = document.querySelector('form');
 const inputs = document.querySelectorAll('input[type="text"], input[type="password"]');
 let pseudo, email, password, confirmPass;
+const progressBar = document.getElementById('progress-bar');
 
 //fonction d'affichage
 const errorDisplay = (tag, message, valid) =>{  
     const container = document.querySelector("." + tag + "-container");
-    const span = document.querySelector("." + tag + "-span");
+    const span = document.querySelector("." + tag + "-container > span");
 
     if (!valid){
         container.classList.add('error');
@@ -29,17 +31,42 @@ const pseudoChecker = (value) => {
 }
 
 const emailChecker = (value) => {
-    console.log(value);
+    if(!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)){
+        errorDisplay('email', 'L\'email est invalide.', false);
+        email = null;
+    }else{
+        errorDisplay('email', '', true);
+        email = value;
+    }
 }
 
 const passwordChecker = (value) => {
-    console.log(value);
+    progressBar.classList="";   //réinit de progressbar pour que les classes ne s'empilent pas.
+    if(!value.match(/^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/)){
+        errorDisplay('password','Minimum 8 caractères, une majuscule, un chiffre et un caractère spécial', false);
+        progressBar.classList.add('progressRed');
+        password=null
+    }else if(value.length < 12){
+        progressBar.classList.add('progressBlue');
+        errorDisplay('password','', true)
+        password=value;
+    }else{
+        progressBar.classList.add('progressGreen');
+        errorDisplay('password','', true)
+        password=value;
+    }
+    if(confirmPass) confirmChecker(confirmPass);
 }
 
 const confirmChecker = (value) => {
-    console.log(value);
+    if(value !== password){
+        errorDisplay('confirm','Les mots de passe ne correspondent pas', false);
+        confirmPass = null;
+    }else{
+        errorDisplay('confirm','', true);
+        confirmPass = value;
+    }
 }
-
 
 inputs.forEach((input)=>{
     input.addEventListener('input',(e)=>{
@@ -64,4 +91,21 @@ inputs.forEach((input)=>{
                 null;
         }
     })
+})
+
+//Envoi du formulaire
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(pseudo && email && password && confirmPass){
+        const data = {pseudo,email,password,}   //exactement identique à la ligne ci-dessous
+        // const data = {pseudo: pseudo,email: email,password: password,} 
+        console.log(data);      //simule l'envoi de la data
+
+        inputs.forEach((input)=>{input.value=''});
+        progressBar.classList= "";
+
+        alert('Inscription validée !');
+    }else{
+        alert('Veuillez remplir correctement les champs')
+    }
 })
