f78cf3ee2bf39d1adaff7d2c013e3a6fa883fa9c
maedial <adrien.lomas@maedial.com>
2022-06-27 16:56:48 +0200

    algo tris

---
+++ Algo/bubble/bubble.js
@@ -0,0 +1,14 @@
+function bubbleSort(array){
+    for (let i=0; i< array.length-1; i++){
+        for (let j = 0; j < array.length -1 -i; j++){
+            if(array[j] > array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    console.log(array);
+}
+
+bubbleSort([451,2,65,4,7,98,2,3,1]);
